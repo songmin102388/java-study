794499fae413b9724ea2d5d639a832c6b8348bab
songmin102388 <songmin1023@icloud.com>
2025-09-03 12:32:03 +0900

    0903 이송민

--- chap04/src/sec02/exam09/BreakOutterExample.java
+++ chap04/src/sec02/exam09/BreakOutterExample.java
@@ -2,8 +2,17 @@ package sec02.exam09;
 
 public class BreakOutterExample {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+		Outter:
+		for(char upper='A'; upper<='Z'; upper++) {
+			for(char lower='a'; lower<='z'; lower++) {
+				System.out.println(upper + "_" + lower);
+				if(lower=='g') {
+					break Outter;
+				}
+			}
+		}
+		System.out.println("프로그램 실행 종");
 
 	}
 
--- chap04/src/sec02/exam10/ContinueExample.java
+++ chap04/src/sec02/exam10/ContinueExample.java
@@ -2,8 +2,13 @@ package sec02.exam10;
 
 public class ContinueExample {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws Exception {
+		for(int i=1;i<=10; i++) {
+			if(i%2 != 0) {
+				continue;
+			}
+			System.out.println(i);
+		}
 
 	}
 
