b9a28f32458036c7009b81d4b659d2d356a22cf2
songmin102388 <songmin1023@icloud.com>
2025-11-24 12:31:06 +0900

    1124

---
+++ Cote/src/cote/exam1124/Exam1.java
@@ -0,0 +1,26 @@
+package cote.exam1124;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exam1 {
+	public static void main(String[] args) {
+		int[] values  = {1,3,5,7,9};
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("인텍스를 선택하세요");
+		try {
+			int index = scanner.nextInt();
+			System.out.println(values[index]);
+		}
+		catch(InputMismatchException e){
+			System.out.println("숫자 써라 ㅅㅂ년아");
+			
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("범위 안에서 입력해라");
+			System.out.println(values[0]);
+		}
+		scanner.close();
+		
+	}
+}
---
+++ Cote/src/cote/exam1124/Exam2.java
@@ -0,0 +1,10 @@
+package cote.exam1124;
+
+public class Exam2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
